7f3953a7aef217051f1eef8076b705741e972faa
daisyleenr <daisyleenr@gmail.com>
2015-12-05 02:03:55 +0900

    const add

--- front/auth/check_password.js
+++ front/auth/check_password.js
@@ -1,7 +1,9 @@
-const hashstr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-const iter = 12000;
-
 var pbkdf2 = require('pbkdf2-sha256');
+
+const HASHSTR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const ITER = 12000;
+const BEFOREITER = 'pbkdf2_sha256';
+
 /* *
  * key: originPassword
  * string: `pdkdf2-sha256` password
@@ -20,8 +22,8 @@ exports.hashSync = function(key){
     var result = "";
     var salt = makeSalt();
 
-    result = 'pbkdf2_sha256$' + iter + '$' + salt + '$';
-    result += pbkdf2(key, new Buffer(salt), iter, 32).toString('base64');
+    result = BEFOREITER + '$' + ITER + '$' + salt + '$';
+    result += pbkdf2(key, new Buffer(salt), ITER, 32).toString('base64');
 
     return result;
 }
@@ -29,7 +31,7 @@ exports.hashSync = function(key){
 function makeSalt(){
     var salt = "";
     for (var i = 0; i < 32; i++) {
-        salt += hashstr[Math.floor((Math.random() * 62))];
+        salt += HASHSTR[Math.floor((Math.random() * 62))];
     }
     return salt;
 }
